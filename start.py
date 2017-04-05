297c7aa547648e2bf9dc222d06765beb0a455123
Yu-Hsuan <yu-hsuan@Yu-Hsuande-MacBook-Pro.local>
2017-04-05 16:27:15 +0800

    get token with python

--- start.py
+++ start.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
+from watson_developer_cloud import AuthorizationV1
+from watson_developer_cloud import SpeechToTextV1
 
 app = Flask(__name__)
 app.config.update(
@@ -8,8 +11,17 @@ app.config.update(
 )
 
 @app.route("/")
-def index():
-	return render_template('index.html')
+def index(token = None):
+	authorization = AuthorizationV1(
+	    username='1aa8c4e0-7be1-4a39-9316-50f5014dbda7',
+	    password='jiBwnAiiMF3w'
+	)
+
+	# print(json.dumps(authorization.get_token(url=SpeechToTextV1.default_url), indent=2))
+	# token = json.dumps(authorization.get_token(url=SpeechToTextV1.default_url), indent=2)
+	token = authorization.get_token(url=SpeechToTextV1.default_url)
+	print(token)	
+	return render_template('index.html', token = token)
 
 
 if __name__ == "__main__":
