45ec166d3d3a6fef0e65abda9ae118d046c37060
abq8392 <abq8392@hotmail.com>
2017-05-02 18:35:03 +0800

    writefile

--- static/js/test.js
+++ static/js/test.js
@@ -1,5 +1,6 @@
 var aaa;
 var start_timestamp;
+
 function start() {
     var token = $('#tokenDiv').text().trim();
     // var token = 'I4RLzAC4Ev4ZgnjYvePuNIFpGy%2FDttL5U%2F6bMWQu0tcbz2G5m2euximrwlv7fRG6324EfEl4sC%2FTkEgIeglwrgUPYKb4iv9EL7rld62TvU3vc9PrindyNELnMG7IiEFOGZczaEMFXTwmQdwtEfjc3Gxvczw1rg1x5jsB5ueZ0NnMSYA0SbrHlf7B%2BdrHlVg3CMCTeGembcz6h%2FzmMDDZYrK0ncWajUT8aEvsgrFnLbCgGlkKiY1Uijs291sSPJamcT0STJPE5aARBYIjQkX7wTbqXVbyuD%2BPx3Zlp6QnaUK%2BxuteHKigyQgyd9jaBra0hHcBVzx6NthB6Fe1F4uNAWmUNx1tQpjeLrIZE1s%2BK1zZsVrsqxuMs%2FlrsvlL3PcpunyWwEiD7zL0ud62gT45cO%2Boy2LZQVWKn5W3NrMLTY1kZ7hzYVanBBlT2I9u1s1uqU2WzcB9aLelPPQCndbWz7uqBo4YSUb4qd1LwpWmjggURgSk47I%2FID50%2FNaA0GnTfd6Rw6pow%2Bw0aUo8UsucA%2BBt8CjOUuR8Y2i2FL7AACHMLk7Nk8QN1BpJwn9Jja%2FqRMuSLeY2jYkHgPtPz6wbl12Rw7SIn%2BIf0ZjeoeZ21z56eqyX57I%2FEz9LN5KOyIM0klAmS8eENB6EpVJxCxgsz4b6Jxa%2BuLAbCOZYssJJGORdz4%2BR0E%2B49WjBMU2%2FYS1icwG6FGfuB72q22OROBZqXMq3hAxMyp1dnqJV90%2FxHvd0e25U0r4kPd3oB8BBkBZFG5KUgpuV%2FDYoFQs1mKQ3dOT95VbWGtY%2F22dyfqk1TJKBJYgM5pcL6ZNZSYNuPZvxg%2BLAgwBj%2BM8ijVEv7lgb3NbR070IdogB3RqqSLvUBh1b6oOoSNqTZDH0mf31bmXaN6Oq6H2xeLCZupo3pI%2FA645JW5X%2FLed1FI0ETfIGX%2FMFZPb6OYAYmNl0hcgDBpT8';
@@ -7,7 +8,7 @@ function start() {
     //console.log('hihi');
 
     // Get unix timestamp
-    start_timestamp = Math.round(+new Date()/1000);
+    start_timestamp = Math.round(+new Date() / 1000);
     console.log(start_timestamp);
 
 
@@ -29,7 +30,7 @@ function start() {
     stream.on('data', function(data) {
 
         if (data.results[0].final == true) {
-            
+
             console.log("initial data: \n" + JSON.stringify(data, null, 4));
 
             $('#test' + lastDivId).text(data.results[0].alternatives[0].transcript);
@@ -48,17 +49,17 @@ function start() {
 
             // Get finish detecting sentences time.
             var sentence_length = data.results[0].alternatives[0].timestamps.length;
-            var end_timestamp = Math.round(+new Date()/1000);
+            var end_timestamp = Math.round(+new Date() / 1000);
             console.log("original end time: " + end_timestamp);
             console.log("start time in function: " + start_timestamp);
-            
-            var last_detect_word_time = parseFloat(data.results[0].alternatives[0].timestamps[sentence_length-1][2]);
+
+            var last_detect_word_time = parseFloat(data.results[0].alternatives[0].timestamps[sentence_length - 1][2]);
             console.log("last detect word:" + last_detect_word_time);
-            
+
             last_detect_word_time += start_timestamp;
             var delayTime = end_timestamp - last_detect_word_time;
-            
-           console.log("DelayTime: " + delayTime);
+
+            console.log("DelayTime: " + delayTime);
 
         }
 
@@ -71,4 +72,24 @@ function start() {
         console.log('end');
     });
     document.querySelector('#stopBtn').onclick = stream.stop.bind(stream);
+}
+
+function WriteToFile() {
+    var fso = new ActiveXObject('Scripting.FileSystemObject');
+
+    var dateObj = new Date();
+    var temp = '';
+    temp = temp + dateObj.getYear();
+    temp = temp + (dateObj.getMonth() + 1);
+    temp = temp + dateObj.getDate();
+    temp = temp + dateObj.getHours();
+    temp = temp + dateObj.getMinutes();
+    temp = temp + dateObj.getSeconds();
+
+    var folder = 'DelayTimeLog_' + temp + '.txt';
+
+    var s = fso.CreateTextFile(folder, true);
+    
+    s.WriteLine(text);
+    s.Close();
 }
